bea7e7f2e6b4d54baacbe67dd68db273876b3412
Juliann Fields <t-jufiel@microsoft.com>
2017-06-23 12:30:36 -0700

    create messageload class

--- AzureFunctions.AngularClient/src/app/shared/models/localStorage/local-storage.ts
+++ AzureFunctions.AngularClient/src/app/shared/models/localStorage/local-storage.ts
@@ -1,5 +1,6 @@
 import { StorageItem } from './local-storage';
 import {EnabledFeature} from './enabled-features';
+import { Guid } from "app/shared/Utilities/Guid";
 
 export interface StorageItem{
     id : string;
@@ -13,6 +14,11 @@ export interface QuickstartSettings extends StorageItem{
     disabled : boolean;
 }
 
+export class MessageLoad {
+    id : Guid;
+    verb : string;
+    data : any;
+}
 export interface TabSettings extends StorageItem{
     dynamicTabId : string;
 } 
